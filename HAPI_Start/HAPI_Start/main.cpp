07815456d484168da12746b1e5101f492ecc505c
Liam O'Neil <liam.oneil.98@gmail.com>
2017-10-16 21:26:57 +0100

    Milestone 2(Pre-edit)

--- HAPI_Start/HAPI_Start/main.cpp
+++ HAPI_Start/HAPI_Start/main.cpp
@@ -20,13 +20,13 @@ Additionally in the top directory there are two batch files for creating a redis
 using namespace HAPISPACE;
 void ClearToColour(BYTE r, BYTE g, BYTE b, BYTE* screen, int width, int height);
 
-struct Star
-{
-	float starX;
-	float starY;
-	float starZ;
-};
-void DrawStars(Star star, BYTE* screen, int width, int height, BYTE r, BYTE g, BYTE b, float eyeDist);
+//struct Star
+//{
+//	float starX;
+//	float starY;
+//	float starZ;
+//};
+//void DrawStars(Star star, BYTE* screen, int width, int height, BYTE r, BYTE g, BYTE b, float eyeDist);
 // Every HAPI program has a HAPI_Main as an entry point
 // When this function exits the program will close down
 void HAPI_Main()
@@ -44,18 +44,18 @@ void HAPI_Main()
 		return;
 
 	BYTE* screen = HAPI.GetScreenPointer();
-	const int numStar = 100000;
+	//const int numStar = 100000;
 	HAPI.SetShowFPS(true);
-	Star* stars = new Star[numStar]();
+	//Star* stars = new Star[numStar]();
 
 	
-	for (int i = 0; i < numStar; i++)
-	{
-		stars[i].starX = rand() % width;
-		stars[i].starY = rand() % height;
-		stars[i].starZ = rand() % 100;
-	}
-	float eyeDist = 100.f;
+	//for (int i = 0; i < numStar; i++)
+	//{
+	//	stars[i].starX = rand() % width;
+	//	stars[i].starY = rand() % height;
+	//	stars[i].starZ = rand() % 100;
+	//}
+	//float eyeDist = 100.f;
 	while (HAPI.Update())
 	{
 		/*HAPI_TMouseData mouse = HAPI.GetMouseData();
@@ -67,30 +67,30 @@ void HAPI_Main()
 		//HAPI.SetControllerRumble(0, 65535, 65535);
 		
 		HAPI_TKeyboardData keyboard = HAPI.GetKeyboardData();
-		if (keyboard.scanCode['S'])
-		{
-			eyeDist += 1;
-			if (eyeDist > 500)
-			{
-				eyeDist = 500;
-			}
-			
-		}
-		if (keyboard.scanCode['W'])
-		{
-			eyeDist -= 1;
-			if (eyeDist < -50)
-			{
-				eyeDist = -50;
-			}
-		
-		}
+		//if (keyboard.scanCode['S'])
+		//{
+		//	eyeDist += 1;
+		//	if (eyeDist > 500)
+		//	{
+		//		eyeDist = 500;
+		//	}
+		//	
+		//}
+		//if (keyboard.scanCode['W'])
+		//{
+		//	eyeDist -= 1;
+		//	if (eyeDist < -50)
+		//	{
+		//		eyeDist = -50;
+		//	}
+		//
+		//}
 		if (keyboard.scanCode[HK_ESCAPE])
 		{
 			HAPI.Close();
 		}
 		ClearToColour(0, 0, 50, screen, width, height);
-		for (int i = 0; i < numStar; i++)
+		/*for (int i = 0; i < numStar; i++)
 		{
 			int colourPick = std::rand() % 4;
 			if (colourPick == 0)
@@ -116,12 +116,12 @@ void HAPI_Main()
 				stars[i].starZ = 100;
 			}
 			
-		}
+		}*/
 		
 
 		//memset(screen, 150, width*height * 4);
 	}
-	delete[] stars;
+	//delete[] stars;
 	//HAPI.SetControllerRumble(0, 0, 0);
 
 }
@@ -139,21 +139,21 @@ void ClearToColour(BYTE r, BYTE g, BYTE b, BYTE* screen, int width, int height)
 //Sx = ((eyeDist * (Px - Cx)) / (eyeDist + Pz)) + Cx
 //Sy = ((eyeDist * (Py - Cy)) / (eyeDist + Pz)) + Cy
 
-void DrawStars(Star star, BYTE* screen, int width, int height, BYTE r, BYTE g, BYTE b, float eyeDist)
-{
-	//float eyeDist = 100;
-	float Sx = ((eyeDist*(star.starX - width / 2)) / (eyeDist + star.starZ)) + width / 2;
-	float Sy = ((eyeDist*(star.starY - height / 2)) / (eyeDist + star.starZ)) + height / 2;
-	int i = ((int)Sx + ((int)Sy*width)) * 4;
-	if (Sx >= width || Sy >= height || Sx <= 0 || Sy <= 0)
-	{
-		return;
-	}
-	screen[i] = r;
-	screen[i + 1] = g;
-	screen[i + 2] = b;
-
-
-
-
-}
+//void DrawStars(Star star, BYTE* screen, int width, int height, BYTE r, BYTE g, BYTE b, float eyeDist)
+//{
+//	//float eyeDist = 100;
+//	float Sx = ((eyeDist*(star.starX - width / 2)) / (eyeDist + star.starZ)) + width / 2;
+//	float Sy = ((eyeDist*(star.starY - height / 2)) / (eyeDist + star.starZ)) + height / 2;
+//	int i = ((int)Sx + ((int)Sy*width)) * 4;
+//	if (Sx >= width || Sy >= height || Sx <= 0 || Sy <= 0)
+//	{
+//		return;
+//	}
+//	screen[i] = r;
+//	screen[i + 1] = g;
+//	screen[i + 2] = b;
+//
+//
+//
+//
+//}
