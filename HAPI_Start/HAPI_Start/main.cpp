0c4e909b3176a1e6c75e230c5b392d337c5a8d9f
Liam O'Neil <liam.oneil.98@gmail.com>
2017-10-16 22:42:15 +0100

    FastBlit Added

--- HAPI_Start/HAPI_Start/main.cpp
+++ HAPI_Start/HAPI_Start/main.cpp
@@ -2,18 +2,24 @@
 
 
 using namespace HAPISPACE;
-void ClearToColour(BYTE r, BYTE g, BYTE b, BYTE* screen, int width, int height);
+void ClearToColour(BYTE r, BYTE g, BYTE b, BYTE* screen, int width, int height);void FastBlit(BYTE* screenPointer, BYTE* texturePointer, int screenHeight, int textureHeight, int screenWidth, int textureWidth);
 
 void HAPI_Main()
 {
-	int width(1920*2);
-	int height(1200);
+	int width(1280);
+	int height(720);
 	
 	HAPI_TColour textfill(HAPI_TColour::CYAN);
 
 
 	if (!HAPI.Initialise(width, height, "The little HAPI window that could"))
 		return;
+
+	BYTE *texPoint;
+	int texWidth, texHeight;
+	if (!HAPI.LoadTexture("Data\\alphaThing.tga", &texPoint, texWidth, texHeight))
+		return;
+
 
 	BYTE* screen = HAPI.GetScreenPointer();
 	HAPI.SetShowFPS(true);
@@ -28,7 +34,10 @@ void HAPI_Main()
 			HAPI.Close();
 		}
 		ClearToColour(0, 0, 50, screen, width, height);
+		FastBlit(screen, texPoint, height, texHeight, width, texWidth);
+		
 	}
+	delete[] texPoint;
 }
 
 void ClearToColour(BYTE r, BYTE g, BYTE b, BYTE* screen, int width, int height)
@@ -40,3 +49,10 @@ void ClearToColour(BYTE r, BYTE g, BYTE b, BYTE* screen, int width, int height)
 		screen[i + 2] = b;
 	}
 }
+void FastBlit(BYTE* screenPointer, BYTE* texturePointer, int screenHeight, int textureHeight, int screenWidth, int textureWidth){
+	for (int y = 0; y < textureHeight; y++)
+	{
+		memcpy(screenPointer, texturePointer, textureHeight * 4);
+		screenPointer = screenPointer + screenWidth * 4;
+		texturePointer = texturePointer + textureWidth * 4;
+	}}
